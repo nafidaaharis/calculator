9d3f27418126e7e328ff9eced7baad866663e123
nafidaaharis <nafithaharis7736@gmail.com>
2023-08-24 16:20:22 +0530

    first commit

---
+++ calcu.js
@@ -0,0 +1,16 @@
+function display(num){
+
+    output.value +=num;
+}
+
+function clearAll(){
+    output.value=''
+} 
+
+function evaluateExp(){
+    output.value=eval(output.value)
+}
+function removeLast(){
+    currentExp=output.value
+    output.value=currentExp.slice(0,-1)
+}
